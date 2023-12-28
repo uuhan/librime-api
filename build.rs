02ac014bbb0a370ef7826cacd549f5928e0e6211
uuhan <xuminhui189@gmail.com>
2023-12-28 21:05:19 +0800

    librime static build

--- build.rs
+++ build.rs
@@ -8,8 +8,9 @@ fn main() {
 
     let dst = Config::new("librime")
         .define("BUILD_TEST", "0")
-        .define("BUILD_SHARED_LIBS", "1")
-        .define("BUILD_STATIC", "0")
+        .define("BUILD_SHARED_LIBS", "0")
+        .define("ENABLE_LOGGING", "1")
+        .define("BUILD_STATIC", "1")
         .define("CMAKE_BUILD_TYPE", "Release")
         .build();
 
@@ -25,6 +26,13 @@ fn main() {
         .write_to_file(out.join("api.rs"))
         .expect("could not write bindings!");
 
+    println!("cargo:rustc-flags=-l dylib=c++");
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=dylib=rime");
+    println!("cargo:rustc-link-search=native=librime/lib");
+    println!("cargo:rustc-link-lib=static=rime");
+    println!("cargo:rustc-link-lib=static=glog");
+    println!("cargo:rustc-link-lib=static=yaml-cpp");
+    println!("cargo:rustc-link-lib=static=marisa");
+    println!("cargo:rustc-link-lib=static=opencc");
+    println!("cargo:rustc-link-lib=static=leveldb");
 }
